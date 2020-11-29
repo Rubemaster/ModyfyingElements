d394ae334b00e85460593a8c1d2827f7d14d20e5
Ruben Gustaf Ingemar RIck <ruben.rick@outlook.com>
2020-11-29 14:13:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  const phreshStyle={
+    backgroundColor: "green"
+  }
+  return (
+    <div className="App">
+      <h1 style={phreshStyle}>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
